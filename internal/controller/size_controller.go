d3bcdc34d1c01c2591dcb8c1ea1db16af060dd9b
Artem Bortnikov <artem.bortnikov@telekom.com>
2024-06-13 17:56:59 +0300

    size controller
    Signed-off-by: Artem Bortnikov <artem.bortnikov@telekom.com>

--- internal/controller/size_controller.go
+++ internal/controller/size_controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 
 	metalv1alpha1 "github.com/ironcore-dev/metal/api/v1alpha1"
+	metalv1alpha1apply "github.com/ironcore-dev/metal/client/applyconfiguration/api/v1alpha1"
+	"github.com/ironcore-dev/metal/internal/log"
+	"github.com/ironcore-dev/metal/internal/ssa"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -39,11 +42,105 @@ func (r *SizeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.
 		return ctrl.Result{}, r.finalize(ctx, size)
 	}
 
-	return ctrl.Result{}, nil
+	return ctrl.Result{}, r.reconcile(ctx, size)
+}
+
+func (r *SizeReconciler) reconcile(ctx context.Context, size metalv1alpha1.Size) error {
+	if !controllerutil.ContainsFinalizer(&size, SizeFinalizer) {
+		sizeApply := metalv1alpha1apply.Size(size.Name, size.Namespace).
+			WithFinalizers(SizeFinalizer)
+		return r.Patch(ctx, &size, ssa.Apply(sizeApply), client.FieldOwner(SizeFieldOwner), client.ForceOwnership)
+	}
+
+	inventories := &metalv1alpha1.InventoryList{}
+	if err := r.List(ctx, inventories); err != nil {
+		log.Error(ctx, fmt.Errorf("failed to list inventories: %w", err))
+		return err
+	}
+
+	for _, inventory := range inventories.Items {
+		if !inventory.DeletionTimestamp.IsZero() {
+			continue
+		}
+
+		var labels map[string]string
+		if inventory.Labels == nil {
+			labels = make(map[string]string)
+		} else {
+			labels = inventory.Labels
+		}
+
+		matches, err := size.Matches(&inventory)
+		if err != nil {
+			log.Error(ctx, fmt.Errorf("failed to match size: %w", err))
+		}
+		sizeLabel := size.GetMatchLabel()
+		_, labelPresent := labels[sizeLabel]
+
+		switch matches {
+		case true:
+			if labelPresent {
+				log.Debug(ctx, "match between inventory and size found, label present, will not update")
+				continue
+			} else {
+				log.Info(ctx, "match between inventory and size found")
+				labels[sizeLabel] = "true"
+			}
+		case false:
+			if labelPresent {
+				log.Info(ctx, "inventory no longer matches to size, will remove label")
+				delete(labels, sizeLabel)
+			} else {
+				log.Debug(ctx, "match between inventory and size is not found, label absent, will not update")
+				continue
+			}
+		}
+
+		inventoryApply := metalv1alpha1apply.Inventory(inventory.Name, inventory.Namespace).
+			WithLabels(labels)
+		err = r.Patch(
+			ctx, &inventory, ssa.Apply(inventoryApply), client.FieldOwner(SizeFieldOwner), client.ForceOwnership)
+		if err != nil {
+			log.Error(ctx, fmt.Errorf("failed to patch inventory: %w", err))
+		}
+	}
+
+	return nil
 }
 
 func (r *SizeReconciler) finalize(ctx context.Context, size metalv1alpha1.Size) error {
-	return nil
+	inventories := &metalv1alpha1.InventoryList{}
+	if err := r.List(ctx, inventories); err != nil {
+		log.Error(ctx, fmt.Errorf("failed to list inventories: %w", err))
+		return err
+	}
+
+	sizeLabel := size.GetMatchLabel()
+	for _, inventory := range inventories.Items {
+		var labels map[string]string
+		if inventory.Labels == nil {
+			labels = make(map[string]string)
+		} else {
+			labels = inventory.Labels
+		}
+
+		if _, labelPresent := labels[sizeLabel]; !labelPresent {
+			continue
+		}
+
+		delete(labels, sizeLabel)
+		inventoryApply := metalv1alpha1apply.Inventory(inventory.Name, inventory.Namespace).
+			WithLabels(labels)
+		err := r.Patch(
+			ctx, &inventory, ssa.Apply(inventoryApply), client.FieldOwner(SizeFieldOwner), client.ForceOwnership)
+		if err != nil {
+			log.Error(ctx, fmt.Errorf("failed to patch inventory: %w", err))
+		}
+	}
+
+	sizeApply := metalv1alpha1apply.Size(size.Name, size.Namespace).
+		WithFinalizers()
+	return r.Patch(ctx, &size, ssa.Apply(sizeApply), client.FieldOwner(SizeFieldOwner), client.ForceOwnership)
 }
 
 func (r *SizeReconciler) SetupWithManager(mgr ctrl.Manager) error {
